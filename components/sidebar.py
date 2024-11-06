2ae280dc7c29ecb9415523732550c65da9f777b4
Souraya Ahmed <99414919+Sahm269@users.noreply.github.com>
2024-11-06 17:40:14 +0100

    side bar a changé

--- components/sidebar.py
+++ components/sidebar.py
@@ -18,10 +18,11 @@ def create_sidebar(collapsed):
           dbc.Nav(
                 [
                     dbc.NavLink([html.I(className="fas fa-home"), html.Span("Home")], href="/", active="exact"),
-                    dbc.NavLink([html.I(className="fas fa-chart-line"), html.Span("Prédiction")], href="/prediction", active="exact"),
-                    dbc.NavLink([html.I(className="fas fa-map-marker-alt"), html.Span("Map")], href="/map", active="exact"),
                     dbc.NavLink([html.I(className="fas fa-chart-bar"), html.Span("Contexte")], href="/contexte", active="exact"),
+                    dbc.NavLink([html.I(className="fas fa-map-marker-alt"), html.Span("Map")], href="/map", active="exact"),
                     dbc.NavLink([html.I(className="fas fa-chart-pie"), html.Span("Analytiques")], href="/analytiques", active="exact"),
+                    dbc.NavLink([html.I(className="fas fa-chart-line"), html.Span("Prédiction")], href="/prediction", active="exact"),
+                 
                 ],
                 vertical=True,
                 pills=True,
